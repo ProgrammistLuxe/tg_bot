a4c6caeff02afc9114100e2576930c2eb1d66552
Kovalchuk <kovalchuk.ia@intechs.by>
2024-09-30 17:01:49 +0300

    improve show list util

--- src/utils/show-list.ts
+++ src/utils/show-list.ts
@@ -1,5 +1,5 @@
 import { Subject } from '@api/subject/entities/subject.entity';
 
 export function showList(list: Subject[]) {
-  return `${list.map((subject) => `${subject.name}, вариант ${subject.variant} лаб сдано: ${subject.labs_done}/${subject.labs_all}\n`).join('')}`;
+  return `${list.map((subject) => `${subject.name}, вариант ${subject.variant}, лаб сдано: ${subject.labs_done}/${subject.labs_all}\n`).join('')}`;
 }
